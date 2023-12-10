1a05539e5176b21d290e145074185e215049fa9a
Astrash <astrash@protonmail.com>
2023-12-10 16:54:25 +1100

    Separate config doc yaml into multiple files

---
+++ docs/config/documentation/addons/README.md
@@ -0,0 +1,6 @@
+# Addon Documentation
+
+The files in this directory define configuration documentation for each addon.
+The files are read by the configdocumenter extension defined in docs/ext and
+generate documentation into the docs/config/documentation/objects directory.
+
--- docs/ext/configdocumenter.py
+++ docs/ext/configdocumenter.py
@@ -7,17 +7,26 @@ from configobjects import *
 
 def setup(app):
     doc_dir = os.path.join(app.srcdir, 'config/documentation')
-    doc_config  = os.path.join(doc_dir, 'objects.yml')
+    addons_dir = os.path.join(doc_dir, 'addons')
     output_dir = os.path.join(doc_dir, 'objects')
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    with open(doc_config, 'r') as file:
-        docs = yaml.safe_load(file)
+    addons = {}
+
+    for filename in os.listdir(addons_dir):
+        file = os.path.join(addons_dir, filename)
+        if not filename.endswith(".yml") or not os.path.isfile(file):
+            continue
+        with open(file, 'r') as file:
+            for addon_name, addon in yaml.safe_load(file).items():
+                if addon_name in addons:
+                    raise ValueError(f"Addon {addon_name} is defined multiple times across multiple files")
+                addons[addon_name] = addon
 
     objects = {}
 
     # Create objects
-    for addon_name, addon in docs.items():
+    for addon_name, addon in addons.items():
         for object_name, object_description in addon.get("objects", {}).items():
             if object_name in objects:
                 raise ValueError(f"Multiple addons define a description for object {object_name}")
@@ -25,7 +34,7 @@ def setup(app):
             objects[object_name] = type_keys[object_type](object_description)
 
     # Add templates to templated objects
-    for addon_name, addon in docs.items():
+    for addon_name, addon in addons.items():
         for object_name, templates in addon.get("templates", {}).items(): 
             if object_name not in objects:
                 continue
